8c4d107f875ee39e72b0d40ff5dc7acb443c5414
HBKLXNDR <hbklksndr@protonmail.com>
2023-01-10 19:42:43 +0200

    done

--- app.js
+++ app.js
@@ -1,91 +1,80 @@
-// const form = document.querySelector('.todo-form');
-// const input = document.querySelector('.todo-input');
-// const itemsList = document.querySelector('.todo-items');
-// let todos = [];
-//
-// form.addEventListener('submit', function (event) {
-//     event.preventDefault();
-//     addTodo(input.value);
-// });
-//
-// function addTodo(item) {
-//     if (item !== '') {
-//         const todo = {
-//             id: Date.now(),
-//             name: item,
-//             completed: false
-//         };
-//
-//         todos.push(todo);
-//         addToLocalStorage(todos);
-//         item.value = '';
-//     }
-// }
-//
-// function renderTodos(todos) {
-//     itemsList.innerHTML = '';
-//     todos.forEach(function (item) {
-//         const checked = item.completed ? 'checked' : null;
-//         const li = document.createElement('li');
-//         li.setAttribute('class', 'item');
-//         li.setAttribute('data-key', item.id);
-//
-//         if (item.completed === true) {
-//             li.classList.add('checked');
-//         }
-//
-//         li.innerHTML = `
-//       <input type="checkbox" class="checkbox" ${checked}>
-//       ${item.name}
-//       <button class="delete-button">X</button>
-//     `;
-//
-//         itemsList.append(li);
-//     });
-// }
-//
-// function addToLocalStorage(todos) {
-//     localStorage.setItem('todos', JSON.stringify(todos));
-//     renderTodos(todos);
-// }
-//
-// function getFromLocalStorage() {
-//     const reference = localStorage.getItem('todos');
-//     if (reference) {
-//         todos = JSON.parse(reference);
-//         renderTodos(todos);
-//     }
-// }
-//
-// function toggle(id) {
-//     todos.forEach(function (item) {
-//         if (item.id === +id) {
-//             item.completed = !item.completed;
-//         }
-//     });
-//     addToLocalStorage(todos);
-// }
-//
-// function deleteTodo(id) {
-//     todos = todos.filter(function (item) {
-//         return item.id !== +id;
-//     });
-//     addToLocalStorage(todos);
-// }
-//
-// function sorter() {
-//     todos.map(todo=>{
-//
-//     })
-// }
-//
-//
-// getFromLocalStorage();
-// itemsList.addEventListener('click', function (event) {
-//     if (event.target.type === 'checkbox') {
-//         toggle(event.target.parentElement.getAttribute('data-key'));
-//     }
-//     if (event.target.classList.contains('delete-button')) {
-//         deleteTodo(event.target.parentElement.getAttribute('data-key'));
-//     }
-// });
+const form = document.querySelector("form");
+const input = document.querySelector(".task_input");
+const taskList = document.querySelector(".todo_list");
+
+let todos = [];
+
+const showTasks = (todos) => {
+    taskList.innerHTML = ""
+    todos.map((task) => {
+        const checked = task.done ? "checked" : null;
+        const div = document.createElement("div");
+        div.setAttribute("class", "flex");
+
+        div.innerHTML = `
+        <li accesskey=${task.id} >${task.name}</li>
+        <input type="checkbox" class="checkbox" ${checked}>
+        <button class="delete_button">delete</button>
+        `
+
+        if (task.done === true) {
+            div.children[0].classList.add("checked");
+        }
+
+        taskList.appendChild(div)
+    })
+}
+
+const createTask = (task) => {
+    if (task) {
+        const todo = {
+            id: Math.random().toString(10).substring(2, 8),
+            name: task,
+            done: false
+        };
+
+        todos.push(todo);
+        localStorage.setItem('todos', JSON.stringify(todos));
+        showTasks(todos)
+        input.value = ""
+
+    }
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    createTask(input.value)
+})
+
+taskList.addEventListener("click", (event) => {
+    const id = event.target.parentElement.children[0].getAttribute("accesskey");
+    const del = event.target.innerHTML === "delete"
+    const checkbox = event.target.classList.contains("checkbox")
+
+
+    if (del) {
+        todos = todos.filter((item) => {
+            return item.id !== id;
+        });
+
+        localStorage.setItem('todos', JSON.stringify(todos));
+        showTasks(todos);
+    }
+
+    if (checkbox) {
+        todos.map((item) => {
+            if (item.id === id) {
+                item.done = !item.done;
+            }
+        });
+
+        localStorage.setItem('todos', JSON.stringify(todos));
+        showTasks(todos);
+    }
+})
+
+const localTodos = localStorage.getItem('todos')
+if (localTodos) {
+    todos = JSON.parse(localTodos);
+    showTasks(todos);
+}
